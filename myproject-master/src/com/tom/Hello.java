cb0611a8f2d8b70c90f896fa52e7dc76a6a5403e
julie6625 <32478585+julie6625@users.noreply.github.com>
2017-10-03 16:27:18 +0800

    Add files via upload

---
+++ myproject-master/src/com/tom/Hello.java
@@ -0,0 +1,13 @@
+package com.tom;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		//
+		Person p = new Person();
+		p.sayHello();
+		
+	}
+
+}
---
+++ myproject-master/src/com/tom/Person.java
@@ -0,0 +1,13 @@
+package com.tom;
+
+public class Person {
+	public void sayHello(){
+		System.out.println("Hello!");
+	}
+
+	
+	public static void main(String[] args) {
+		Person p = new Person();
+		p.sayHello();
+	}
+}
