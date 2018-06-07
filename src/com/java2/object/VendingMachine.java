986cef6e8a2799dd96f667748548f4b303b063e3
julie6625 <julie4604@gmail.com>
2018-06-08 02:38:56 +0800

    honepractice

---
+++ src/com/java2/object/VendingMachine.java
@@ -0,0 +1,29 @@
+package com.java2.object;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.java2.oo.Drink;
+
+public class VendingMachine {
+	int total = 0;
+	List<Drink> drinks = new ArrayList<>();
+	
+	public void on(){
+		System.out.println("Please insert coins...");
+		for (int i=0; i<drinks.size(); i++){
+			Drink drink = drinks.get(i);
+			System.out.println(drink.getId() + "\t" + drink.getName() + "\t" + drink.getPrice());
+		}
+		Scanner scanner = new Scanner(System.in);
+		int data = -1;
+		while (data != 0){
+			data = scanner.nextInt();
+			switch(data){
+			case 0:
+				break;
+			}
+		}
+	}
+}
