de4a62a44bd9b672d59bdaeb96ce05b73a950c02
julie6625 <32478585+julie6625@users.noreply.github.com>
2018-06-12 16:21:05 +0800

    1.0.3
    finish setting

--- src/com/java2/object/VendingMachine.java
+++ src/com/java2/object/VendingMachine.java
@@ -1,5 +1,40 @@
 package com.java2.object;
 
-public class VendingMachine {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+
+
+public class VendingMachine {
+	List<Drink> drinks = new ArrayList<>();
+	
+	public VendingMachine(){
+		
+	}
+	
+	public void addDrink(int id, String name, int price) {
+		drinks.add(new Drink(id, name, price));
+	}
+
+	public void on() {
+		Scanner sc1 = new Scanner(System.in);
+		int coinin = sc1.nextInt();
+		boolean c1 = true;
+		boolean c2 = false;
+		int coin = 0;
+		for (int i = 0; i < drinks.size(); i++) {
+			Drink drink = drinks.get(i);
+			System.out.println(drink.getId() + "\t" + drink.getName() + "\t" + drink.getPrice());
+
+		}
+		System.out.println("投幣，或輸入0以結束");
+		
+		while(c1){
+			switch(coinin){
+			
+			}
+			
+		}
+	}	
 }
