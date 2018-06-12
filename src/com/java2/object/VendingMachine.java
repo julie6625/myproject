793fe849475bf6907e6c3e709a1ffb3d8534d401
julie6625 <julie4604@gmail.com>
2018-06-13 00:57:04 +0800

    1.1.0
    finish the whole vending app
    but there're bugs to fix

--- src/com/java2/object/VendingMachine.java
+++ src/com/java2/object/VendingMachine.java
@@ -18,43 +18,80 @@ public class VendingMachine {
 	}
 
 	public void on() {
-		Scanner sc1 = new Scanner(System.in);
-		int coinin = sc1.nextInt();
-		boolean c1 = true;
-		boolean c2 = false;
+		
 		int coin = 0;
+		System.out.println("insert the coin,or enter 0 to the next step");
 		for (int i = 0; i < drinks.size(); i++) {
 			Drink drink = drinks.get(i);
 			System.out.println(drink.getId() + "\t" + drink.getName() + "\t" + drink.getPrice());
 
 		}
-		System.out.println("insert the coin,or enter 0 to quit");
 		
-		while(c1){
+		boolean s1 = true;
+		boolean s2 = false;
+		if(s1){
+			Scanner sc1 = new Scanner(System.in);
+			int coinin = sc1.nextInt();
 			switch(coinin){
 			case 0:
-				System.out.println("quit...");
-				c1=false;
-				c2=true;
+				System.out.println("going to the next step");
+				s1=false;
+				s2=true;
 				break;
 			case 5:
 				coin = coin + 50;
 				System.out.println("coin:"+coin);
-				break;
+				return;
 			case 10:
 				coin = coin + 10;
 				System.out.println("coin:"+coin);
-				break;
+				return;
 			case 50:
 				coin = coin + 50;
 				System.out.println("coin:"+coin);
 				break;
 			default:
 				System.out.println("wrong data,try again.");
-				break;
+				return;
 				
 			}
-			
+		}
+		if(s2) {
+			System.out.println("please enter the drinkID ,enter 0 to quit");
+			Scanner sc2 = new Scanner(System.in);
+			int drinkID = sc2.nextInt();
+			switch(drinkID) {
+			case 0:
+				System.out.println("change :"+coin);
+				break;
+			case 1:
+				if(coin<25) {
+					System.out.println("not enought money..");
+					break;
+				}else {
+					coin = coin - 25;
+					System.out.println("(Coke drops)");
+					System.out.println("money:"+coin);
+				}
+			case 2:
+				if(coin<30) {
+					System.out.println("not enought money..");
+					break;
+				}else {
+					coin = coin - 30;
+					System.out.println("(Orange Juice drops)");
+					System.out.println("money:"+coin);
+				}
+			case 3:
+				if(coin<20) {
+					System.out.println("not enought money..");
+					break;
+				}else {
+					coin = coin - 20;
+					System.out.println("(Sprite drops)");
+					System.out.println("money:"+coin);
+				}
+			}
 		}
 	}	
 }
--- src/com/java2/object/VendingMain.java
+++ src/com/java2/object/VendingMain.java
@@ -1,23 +1,27 @@
 package com.java2.object;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
-import com.java2.oo.Drink;
+import com.java2.object.Drink;
 
 public class VendingMain {
 
 	public static void main(String[] args) {
 		VendingMachine vm = new VendingMachine();
-		FileReader fr;
+		FileInputStream fr;
 		try {
-			fr = new FileReader("vending.txt");
-			BufferedReader in = new BufferedReader(fr);
+			fr = new FileInputStream("vending.txt");
+			InputStreamReader isr = new InputStreamReader(fr);
+			BufferedReader in = new BufferedReader(isr);
 			String line = in.readLine();
 			String[] tokens = line.split(",");
 			int count = Integer.parseInt(tokens[0]);
+			
 			for (int i = 0; i < count; i++){
 				String name = tokens[i*2+1];
 				int price = Integer.parseInt(tokens[i*2+2]);
