2c2428ca47f998b2b730d6fa53d54c8a42e486b6
julie6625 <julie4604@gmail.com>
2018-06-13 18:59:08 +0800

    1.1.1
    issue fixed

--- src/com/java2/object/VendingMachine.java
+++ src/com/java2/object/VendingMachine.java
@@ -29,7 +29,7 @@ public class VendingMachine {
 		
 		boolean s1 = true;
 		boolean s2 = false;
-		if(s1){
+		while(s1){
 			Scanner sc1 = new Scanner(System.in);
 			int coinin = sc1.nextInt();
 			switch(coinin){
@@ -39,58 +39,64 @@ public class VendingMachine {
 				s2=true;
 				break;
 			case 5:
-				coin = coin + 50;
+				coin = coin + 5;
 				System.out.println("coin:"+coin);
-				return;
+				break;
 			case 10:
 				coin = coin + 10;
 				System.out.println("coin:"+coin);
-				return;
+				break;
 			case 50:
 				coin = coin + 50;
 				System.out.println("coin:"+coin);
 				break;
 			default:
 				System.out.println("wrong data,try again.");
-				return;
+				break;
 				
 			}
 		}
-		if(s2) {
+		while(s2) {
 			System.out.println("please enter the drinkID ,enter 0 to quit");
 			Scanner sc2 = new Scanner(System.in);
 			int drinkID = sc2.nextInt();
 			switch(drinkID) {
+			default :
+				System.out.println("wrong data,try again");
 			case 0:
 				System.out.println("change :"+coin);
+				System.out.println("bye bye~");
+				s2 = false;
 				break;
 			case 1:
 				if(coin<25) {
 					System.out.println("not enought money..");
-					break;
+					
 				}else {
 					coin = coin - 25;
 					System.out.println("(Coke drops)");
 					System.out.println("money:"+coin);
 				}
+				break;
 			case 2:
 				if(coin<30) {
 					System.out.println("not enought money..");
-					break;
+					
 				}else {
 					coin = coin - 30;
 					System.out.println("(Orange Juice drops)");
 					System.out.println("money:"+coin);
 				}
+				break;
 			case 3:
 				if(coin<20) {
 					System.out.println("not enought money..");
-					break;
 				}else {
 					coin = coin - 20;
 					System.out.println("(Sprite drops)");
 					System.out.println("money:"+coin);
 				}
+				break;
 			}
 		}
 	}	
