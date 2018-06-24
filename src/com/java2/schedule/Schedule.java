de1b9252b6b27bc7efe3aa974882f55435cacca4
julie6625 <julie4604@gmail.com>
2018-06-24 19:44:23 +0800

    3.0.5
    -t

--- src/com/java2/schedule/Schedule.java
+++ src/com/java2/schedule/Schedule.java
@@ -10,11 +10,23 @@ public class Schedule {
 		System.out.println("請輸入星期(1~7)：");
 		int input = scanner.nextInt();
 		int tmp = 0;
-		int ttime = 0;
-		int tdur = 0;
-		String tcourse = null;
-		String troom = null;
+		int time = 0;
+		int dur = 0;
+		String course = null;
+		String room = null;
 		boolean isavaliable = true;
+		
+		for(int i=0;i<courses.size();i++) {
+			Course c = courses.get(i);
+			if(input==c.getDay()) {
+				tmp = c.getDay();
+				time = c.getTime();
+				dur = c.getDur();
+				room = c.getRoom();
+				course = c.getCourse();
+				break;
+			}
+		}
 		
 		
 		
@@ -22,7 +34,7 @@ public class Schedule {
 		if(isavaliable) {
 			System.out.println("【有空檔】");
 		}else {
-			System.out.println("【有課：   "+troom+" "+tcourse+" 到"+(ttime+tdur)+"時】");
+			System.out.println("【有課：   "+room+" "+course+" 到"+(time+dur)+"時】");
 		}
 	}
 }
