2cf6c06d953fb15c14783a85c410a4a41824fd3b
julie6625 <julie4604@gmail.com>
2018-06-24 19:01:12 +0800

    3.0.2

--- src/com/java2/schedule/Schedule.java
+++ src/com/java2/schedule/Schedule.java
@@ -6,6 +6,14 @@ public class Schedule {
 	List<Course> courses = new ArrayList<>();
 	
 	public void on() {
-		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("請輸入星期(1~7)：");
+		int input = scanner.nextInt();
+		int tmp = 0;
+		int ttime = 0;
+		int tdur = 0;
+		String tcourse = null;
+		String troom = null;
+		boolean isavaliable = true;
 	}
 }
