f0d6c57803d271414343bc2926f9fae726731a80
julie6625 <julie4604@gmail.com>
2018-06-24 19:04:46 +0800

    3.0.3

--- src/com/java2/schedule/Course.java
+++ src/com/java2/schedule/Course.java
@@ -15,5 +15,35 @@ public class Course {
 		this.time = time;
 		this.dur = dur;
 	}
+	public void setRoom(String room) {
+		this.room = room;
+	}
+	public String getRoom() {
+		return room;
+	}
+	public void setcourse(String course) {
+		this.course = course;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setDay(int day) {
+		this.day = day;
+	}
+	public int getDay() {
+		return day;
+	}
+	public void setTime(int time) {
+		this.time = time;
+	}
+	public int getTime() {
+		return time;
+	}
+	public void setDur(int dur) {
+		this.dur = dur;
+	}
+	public int getDur() {
+		return dur;
+}
 
 }
